769309f428f429e51c5407b7a1ce958345860bdc
JesseBB06 <74011261+JesseBB06@users.noreply.github.com>
2020-11-09 18:48:06 +0100

    Update register.php

--- register.php
+++ register.php
@@ -19,6 +19,23 @@ $link = mysqli_connect("localhost",'root','usbw',$database)
     /* Als er op de button 'verzend' geklikt is en wachtwoord komt overeen met wachtwoord2 dan kan je verder */
     if(isset($_POST['verzend'])){
         if($_POST['wachtwoord'] == $_POST['wachtwoord2']){
+            if($_POST['wachtwoord']){
+                /* Variabeles voor de veiligheid */
+                $id  = mysqli_real_escape_string($link,$_POST['id']); 
+                $voornaam  = mysqli_real_escape_string($link,$_POST['voornaam']); 
+                $achternaam  = mysqli_real_escape_string($link,$_POST['achternaam']); 
+                $emailadres  = mysqli_real_escape_string($link,$_POST['emailadres']); 
+                $telefoonnummer  = mysqli_real_escape_string($link,$_POST['telefoonnummer']); 
+                $wachtwoord = mysqli_real_escape_string($link,$_POST['wachtwoord']); 
+                // Validate password strength
+                $uppercase = preg_match('@[A-Z]@', $wachtwoord);
+                $lowercase = preg_match('@[a-z]@', $wachtwoord);
+                $number    = preg_match('@[0-9]@', $wachtwoord);
+                $specialChars = preg_match('@[^\w]@', $wachtwoord);
+
+                if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($wachtwoord) < 8) {
+                    echo "<p style='color:red;'>Je wachtwoord moet minimaal 8 tekens lang zijn, minimaal één hoofdletter, één cijfer en één speciaal teken bevatten.</p>";
+                }else{
             /* Check of het emailadres een geldig emailadres is en check of het emailadres al bekend is in de database. */
             if(!filter_var($_POST['emailadres'], FILTER_VALIDATE_EMAIL)) {
                 exit('Dit is een niet geldig emailadres.');
@@ -27,21 +44,15 @@ $link = mysqli_connect("localhost",'root','usbw',$database)
             if(mysqli_num_rows($select)) {
                 exit('Dit emailadres is al in gebruik.');
             }
-            /* Variabeles voor de veiligheid */
-        $id  = mysqli_real_escape_string($link,$_POST['id']); 
-        $voornaam  = mysqli_real_escape_string($link,$_POST['voornaam']); 
-        $achternaam  = mysqli_real_escape_string($link,$_POST['achternaam']); 
-        $emailadres  = mysqli_real_escape_string($link,$_POST['emailadres']); 
-        $telefoonnummer  = mysqli_real_escape_string($link,$_POST['telefoonnummer']); 
-        $wachtwoord = mysqli_real_escape_string($link,$_POST['wachtwoord']); 
-            
         /* Ingevoerde velden in de database zetten. + encripten van het wachtwoord */
         $result = mysqli_query($link," INSERT INTO `account`(`id`, `voornaam`, `achternaam`, `emailadres`, `telefoonnummer`, `wachtwoord`) VALUES ('$id','$voornaam','$achternaam','$emailadres','$telefoonnummer','".password_hash($wachtwoord, PASSWORD_BCRYPT)."')") 
     or die (mysqli_error($link));
             echo "De gebruiker is aangemaakt.";
         /* echo als de wachtwoorden niet gelijk aan elkaar zijn */
-        }else{
-            echo "De gekozen wachtwoorden zijn niet gelijk aan elkaar.";
+        }
+        }
+    }else{
+            echo "<p style='color:red;'>De gekozen wachtwoorden zijn niet gelijk aan elkaar.</p>";
         }
     }
  ?>
